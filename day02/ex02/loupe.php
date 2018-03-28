a5b9798dd7f14ef41bf1969bfe8d697b8d7bbe43
cvermand <charlottevermandel@gmail.com>
2018-03-28 18:33:39 +0200

    aaa

--- day02/ex02/loupe.php
+++ day02/ex02/loupe.php
@@ -11,7 +11,31 @@ if ($argc > 1)
 		}
 		$elements = $doc->getElementsByTagName("a");
 		foreach($elements as $elem)
-			$elem->nodeValue =  strtoupper($elem->nodeValue) . "\n";
+		{
+			$cpy = $elem->cloneNode(TRUE);
+			$childs = false;
+			if ($cpy->hasChildNodes())
+				$childs = $cpy->childNodes;
+			$elem->textContent = strtoupper($elem->nodeValue);
+			if ($childs)
+			{
+				var_dump($childs->length);
+				if ($childs->length > 1)
+				{
+					$first = true;
+					foreach($childs as $child)
+					{
+					if ($first)
+						$first = false;
+					else
+						$elem->appendChild($child);
+					var_dump($child);
+					}
+				}
+			}
+		}
+		echo $doc->saveHTML();
+			//			$elem->nodeValue =  strtoupper($elem->nodeValue) . "\n";
 		//$doc->saveHTMLfile("new_file.html");
 }
 ?>
